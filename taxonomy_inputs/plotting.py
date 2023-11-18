fc28b7885ace1ab0d181d456ce77f3c3d95571d5
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2023-11-18 16:28:23 +0000

    Plot distributions

--- taxonomy_inputs/plotting.py
+++ taxonomy_inputs/plotting.py
@@ -2,7 +2,7 @@ import os
 
 import pandas as pd
 from matplotlib import pyplot as plt
-from wcvp_download import wcvp_columns
+from wcvp_download import wcvp_columns, plot_native_number_accepted_taxa_in_regions, wcvp_accepted_columns
 
 from taxonomy_inputs import taxonomy_inputs_output_path
 
@@ -38,13 +38,14 @@ def generic_category_plot(var: str, title: str, figsize=(40, 10), sort_var=False
         all_data = all_data.sort_values(var)
 
         # Calculate proportions of each value in the 'Legend' column
-        var_legend_counts = all_data[all_data['Legend'] == 'Ambiguous Homonymous Species Names'].groupby(var).size().reset_index(name='Count_Ambiguous')
+        var_legend_counts = all_data[all_data['Legend'] == 'Ambiguous Homonymous Species Names'].groupby(var).size().reset_index(
+            name='Count_Ambiguous')
 
         # Sort the var column based on the proportions in descending order
         count_sorted_vars = var_legend_counts.sort_values('Count_Ambiguous', ascending=False)[var].tolist()
 
     else:
-        count_sorted_vars =None
+        count_sorted_vars = None
 
     plt.figure(figsize=figsize)
     sns.countplot(x=var, data=all_data, hue='Legend', order=count_sorted_vars)
@@ -52,8 +53,6 @@ def generic_category_plot(var: str, title: str, figsize=(40, 10), sort_var=False
     plt.title(title)
     plt.tight_layout()
     plt.savefig(os.path.join('outputs', 'plots', title + '.jpg'), dpi=300)
-
-
 
     plt.figure(figsize=figsize)
     sns.histplot(data=all_data, x=var, hue='Legend', multiple="fill",
@@ -107,6 +106,7 @@ def family_plots():
     # TODO: maybe change to accepted family?
     generic_category_plot('family', 'WCVP Species Homonyms in Families', figsize=(60, 10), sort_var=True)
 
+
 def plot_taxon_statuses(df: pd.DataFrame, outpath: str, file_tag: str, class_col=wcvp_columns['status'], piefigsize=(4, 2.5)):
     from matplotlib import pyplot as plt
 
@@ -139,9 +139,16 @@ def plot_taxon_statuses(df: pd.DataFrame, outpath: str, file_tag: str, class_col
     plt.savefig(os.path.join(outpath, file_tag + '_bar_chart.png'), dpi=300, bbox_inches='tight', pad_inches=0.1)
     plt.close()
 
+def plot_distributions():
+    ambiguous_homonyms = pd.read_csv(os.path.join(taxonomy_inputs_output_path, 'ambiguous_homonyms', 'homonyms.csv'),
+                                     dtype={'publication_year': 'Int64'})
+    ambiguous_homonyms = ambiguous_homonyms[ambiguous_homonyms[wcvp_columns['status']] == 'Accepted']
+    plot_native_number_accepted_taxa_in_regions(ambiguous_homonyms, wcvp_accepted_columns['species'], os.path.join('outputs', 'plots'),
+                                                'ambiguous_homonyms_dists.jpg', include_extinct=True)
 if __name__ == '__main__':
     year_plots()
     family_plots()
     ambiguous_homonyms = pd.read_csv(os.path.join(taxonomy_inputs_output_path, 'ambiguous_homonyms', 'homonyms.csv'),
                                      dtype={'publication_year': 'Int64'})
-    plot_taxon_statuses(ambiguous_homonyms, os.path.join('outputs', 'plots'),'ambiguous_homonyms_taxon_status')
+    plot_taxon_statuses(ambiguous_homonyms, os.path.join('outputs', 'plots'), 'ambiguous_homonyms_taxon_status')
+    plot_distributions()
